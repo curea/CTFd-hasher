a76f0d34f3a346af50f4dbe4529cf7f9f77790c5
Aaron Cure <aaron.cure@cypressdefense.com>
2017-12-04 08:15:22 -0800

    updated init for readability

--- __init__.py
+++ __init__.py
@@ -14,39 +14,41 @@ def load(app):
 
 	@app.route('/CTFd-hasher', methods=['GET'])
 	def hash():
-		return render_template('page.html', content="<script src='/themes/original/static/js/vendor/jquery.min.js' type='text/javascript'></script>"\
-"<script src='/plugins/CTFd-hasher/assets/md5.min.js' type='text/javascript'></script>"\
-"<script src='/plugins/CTFd-hasher/assets/base64js.min.js' type='text/javascript'></script>"\
-"<script src='/plugins/CTFd-hasher/assets/sha.js' type='text/javascript'></script>"\
-"<h1>Hash a value</h1>"\
-"<input id='valuetohash' style='width: 80%;'></input>&nbsp;"\
-"<button id='hash'>go</button><br/><br/>"\
-"<textarea id='output' style='width: 80%;'></textarea>&nbsp;"\
-"<button id='copy' style='position:relative; top: -8px;'>copy</button>"\
-"<script>"\
-"    $(function() {"\
-"        $('#hash').click( function()"\
-"            {"\
-"                var val = $('#valuetohash').val().trim();\n"\
-"                //var hash = md5(val);\n"\
-"                var shaObj = new jsSHA('SHA-256', 'TEXT');\n"\
-"                shaObj.update(val);\n"\
-"                var hash = shaObj.getHash('HEX');"\
-"                var b64str = base64js.fromByteArray(hash);"\
-"                $('#output').html(b64str);"\
-"            }"\
-"        );"\
-"  "\
-"        $('#copy').click( function()"\
-"            {"\
-"                var $temp = $('<input>');"\
-"                $('body').append($temp);"\
-"                $temp.val($('#output').text()).select();"\
-"                document.execCommand('copy');"\
-"                $temp.remove();"\
-"            }"\
-"        );"\
-"    });"\
-"</script>")
+		return render_template('page.html', content="""
+<script src='/themes/original/static/js/vendor/jquery.min.js' type='text/javascript'></script>
+<script src='/plugins/CTFd-hasher/assets/md5.min.js' type='text/javascript'></script>
+<script src='/plugins/CTFd-hasher/assets/base64js.min.js' type='text/javascript'></script>
+<script src='/plugins/CTFd-hasher/assets/sha.js' type='text/javascript'></script>
+<h1>Hash a value</h1>
+<input id='valuetohash' style='width: 80%;'></input>&nbsp;
+<button id='hash'>go</button><br/><br/>
+<textarea id='output' style='width: 80%;'></textarea>&nbsp;
+<button id='copy' style='position:relative; top: -8px;'>copy</button>
+<script>
+    $(function() {
+        $('#hash').click( function()
+            {
+                var val = $('#valuetohash').val().trim();
+                //var hash = md5(val);
+                var shaObj = new jsSHA('SHA-256', 'TEXT');
+                shaObj.update(val);
+                var hash = shaObj.getHash('HEX');
+                var b64str = base64js.fromByteArray(hash);
+                $('#output').html(b64str);
+            }
+        );
+
+        $('#copy').click( function()
+            {
+                var $temp = $('<input>');
+                $('body').append($temp);
+                $temp.val($('#output').text()).select();
+                document.execCommand('copy');
+                $temp.remove();
+            }
+        );
+    });
+</script>
+""")
 
 	utils.hasher = hash
